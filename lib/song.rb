5099beb71c0a48d73cf2d2b2ee2b2dcc585f5090
Koray Ozkal <koryozkal@gmail.com>
2020-05-04 03:24:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,22 +2,4 @@ require 'pry'
 
 class Song
   extend Memorable::ClassMethods
-  attr_accessor :name
-  attr_reader :artist
-
-  @@songs = []
-
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
-  def self.all
-    @@songs
-  end
-
- 
-
+  
