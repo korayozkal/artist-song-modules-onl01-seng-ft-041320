6694352ec28d6f07b44b0a05ad5af57ed5750659
Koray Ozkal <koryozkal@gmail.com>
2020-05-04 03:16:02 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,16 +1,13 @@
 module Memorable
+  module ClassMethods
 
+    def reset_all
+      all.clear
+    end
 
-def self.reset_all
-self.all.clear
-end
-
-
-def add_song(song)
-@songs << song
-song.artist = self
-end
-
-end
+    def count
+      all.count
+    end
+  end 
 
 
