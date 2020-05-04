199683869fad13acb3de0804f04aea4a4500672d
Koray Ozkal <koryozkal@gmail.com>
2020-05-04 02:26:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,15 +17,15 @@ class Song
   def self.all
     @@songs
   end
+########################
+  #def self.reset_all
+  #  self.all.clear
+  #end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
+  #def self.count
+   # self.all.count
+  #end
+###########################
   def artist=(artist)
     @artist = artist
   end
